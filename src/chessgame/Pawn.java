0b438b916862e98991f3a541acac955fa5472c72
cpfl300 <cpfl300@naver.com>
2013-07-12 15:38:15 +0900

    연습문제 1-4

--- src/chessgame/Pawn.java
+++ src/chessgame/Pawn.java
@@ -2,4 +2,10 @@ package chessgame;
 
 public class Pawn {
 
+	public String getColor() {
+		// TODO Auto-generated method stub
+		String defaultColor =  "white";
+		return defaultColor;
+	}
+
 }
--- test/chessgame/PawnTest.java
+++ test/chessgame/PawnTest.java
@@ -5,5 +5,7 @@ import junit.framework.TestCase;
 public class PawnTest extends TestCase {
 	public void testcreate() throws Exception {
 		Pawn myPawn = new Pawn();
+		String myPawnColor = myPawn.getColor();
+		assertEquals("white", myPawnColor);
 	}
 }
