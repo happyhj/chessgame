e0352e6083cb851734d88c1ed3881f96cb695495
happyhj <happyhj@gmail.com>
2013-07-17 21:59:05 +0900

    연습문제 2-5 (희재,정민)

--- src/chessgame/Board.java
+++ src/chessgame/Board.java
@@ -2,18 +2,34 @@ package chessgame;
 
 import java.util.ArrayList;
 
+/**
+ * 말이 올라가는 보드이다.
+ * @author kimheejae
+ */
 public class Board {
 
 	private ArrayList<Pawn> pawns = new ArrayList<Pawn>();
-
+	
+	/**
+	 * 인자로 받은 말을 말목록에 추가한다.
+	 * @param aPawn
+	 */
 	public void addPawn(Pawn aPawn) {
 		pawns.add(aPawn);
 	}
 
+	/**
+	 * 보드에 추가한 말의 수를 반환한다.
+	 * @return 말의 수
+	 */
 	public int getNumberOfPawns() {
 		return pawns.size();
 	}
 
+	/**
+	 * 지금 보드위에 올라가 있는 말의 목록을 반환한다.
+	 * @return 말의 목록
+	 */
 	public ArrayList<Pawn> getPawnsList() {
 		return pawns;
 	}
--- src/chessgame/Pawn.java
+++ src/chessgame/Pawn.java
@@ -1,20 +1,34 @@
 package chessgame;
 
+/**
+ * 두 색을 가진 말이다.
+ * @author kimheejae
+ */
 public class Pawn {
 // static 이 왜 들어가나?
 	final static String black = "black";
 	final static String white = "white";
 	
 	String color;
-
+	
+	/**
+	 * 색 문자열을 받아 해당색의 말을 만드는 생성자
+	 * @param color
+	 */
 	public Pawn(String color) {
 		this.color = color;
 	}
 
+	/**
+	 * 기본으로 흰색말을 생성하는 생성자
+	 */
 	public Pawn() {
 		color = "white";
 	}
 
+	/**
+	 * @return 말의 색을 반환
+	 */
 	public String getColor() {
 
 		return this.color;
